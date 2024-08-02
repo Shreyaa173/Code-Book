379ee9e35a55781385a178b5cdf21951223bacfe
Shreya <shreya186btcse23@igdtuw.ac.in>
2024-08-02 13:22:10 +0530

    done till testimonals

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
-function toggleDropdown(event) {
-    event.stopPropagation(); // Prevents click event from propagating to the document
-    const dropdown = document.getElementById('dropdown-content');
-    const isDisplayed = dropdown.style.display === 'block';
-    closeAllDropdowns(); // Close other dropdowns if any are open
-    dropdown.style.display = isDisplayed ? 'none' : 'block';
-}
+const carousel = document.querySelector(".carousels");
 
-function closeAllDropdowns() {
-    const dropdowns = document.getElementsByClassName('dropdown-content');
-    for (let i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].style.display = 'none';
-    }
-}
+        let isDragging = false;
+        let startX;
+        let scrollLeft;
 
-window.onclick = function(event) {
-    // Close the dropdown if the click is outside the dropdown container
-    if (!event.target.matches('.dropdown-button')) {
-        closeAllDropdowns();
-    }
-}
+        const dragStart = (e) => {
+            isDragging = true;
+            startX = e.pageX - carousel.offsetLeft;
+            scrollLeft = carousel.scrollLeft;
+            carousel.classList.add("dragging");
+        }
 
-let slideIndex = 0;
-showSlides();
+        const dragging = (e) => {
+            if (!isDragging) return;
+            e.preventDefault();
+            const x = e.pageX - carousel.offsetLeft;
+            const walk = (x - startX) * 20; //scroll-fast
+            carousel.scrollLeft = scrollLeft - walk;
+        }
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+        const dragStop = () => {
+            isDragging = false;
+            carousel.classList.remove("dragging");
+        }
+
+        carousel.addEventListener("mousedown", dragStart);
+        carousel.addEventListener("mousemove", dragging);
+        carousel.addEventListener("mouseup", dragStop);
+        carousel.addEventListener("mouseleave", dragStop);
+
 
 
