eb830d75d7682738530aa99bbebe6f42a6a45f16
saras-69 <115482884+saras-69@users.noreply.github.com>
2024-10-17 01:44:35 +0530

    Created login/signup components and added css files for them

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
 
 function Navbar() {
   return (
     <nav>
-      <div className="nav-icon">
-        <img src={Logo} alt="" className="logo" />
+      <div className='nav-icon'>
+        <img src={Logo} alt='Code Book Logo' className='logo' />
         <p>Code Book</p>
       </div>
-      <div className="nav-text">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <a href="#explore">Courses</a>
+      <div className='nav-text'>
+        <ul className='nav-list'>
+          <li className='nav-item'>
+            <Link to='/login'>Courses</Link>
           </li>
-          <li className="nav-item">
-            <a href="#testimonal">Testimonials</a>
+          <li className='nav-item'>
+            <a href='#testimonal'>Testimonials</a>
           </li>
-          <li className="nav-item">
-            <a href="#contact">Contact</a>
+          <li className='nav-item'>
+            <a href='#contact'>Contact</a>
           </li>
         </ul>
       </div>
-      <div className="nav-button">
-        <button className="btn">
-          <a href="#">
-            Sign-up<i className="fa-solid fa-arrow-right"></i>
-          </a>
+      <div className='nav-button'>
+        <button className='btn'>
+          <Link to='/signup'>
+            Sign-up<i className='fa-solid fa-arrow-right'></i>
+          </Link>
         </button>
-        <button className="btn">
-          <a href="#">
-            Log-in<i className="fa-solid fa-arrow-right"></i>
-          </a>
+        <button className='btn'>
+          <Link to='/login'>
+            Log-in<i className='fa-solid fa-arrow-right'></i>
+          </Link>
         </button>
       </div>
     </nav>
