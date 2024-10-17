45af48a97b17135bb48bb0f17a914ab5d79e0100
srishti <srishtichoudhary52@gmail.com>
2024-10-17 11:48:38 +0530

    hamburger implementation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/logo.png";
 import ProgressBar from "./ProgressBar";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav>
-      <ProgressBar/>
+      <ProgressBar />
       <div className="nav-up">
-      <div className="nav-icon">
-        <img src={Logo} alt="" className="logo" />
-        <p>Code Book</p>
+        <div className="nav-icon">
+          <img src={Logo} alt="Logo" className="logo" />
+          <p>Code Book</p>
+        </div>
+        <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <i className={`fa ${isOpen ? 'fa-times' : 'fa-bars'}`} aria-hidden="true"></i>
+        </div>
       </div>
-      <div className="nav-text">
+      <div className={`nav-content ${isOpen ? 'show' : ''}`}>
         <ul className="nav-list">
           <li className="nav-item">
             <a href="#explore">Courses</a>
@@ -23,22 +33,22 @@ function Navbar() {
             <a href="#contact">Contact</a>
           </li>
         </ul>
-      </div>
-      </div>
-      <div className="nav-button">
-        <button className="btn">
-          <a href="#">
-            Sign-up<i className="fa-solid fa-arrow-right"></i>
-          </a>
-        </button>
-        <button className="btn">
-          <a href="#">
-            Log-in<i className="fa-solid fa-arrow-right"></i>
-          </a>
-        </button>
+        <div className="nav-button">
+          <button className="btn">
+            <a href="#">
+              Sign-up<i className="fa-solid fa-arrow-right"></i>
+            </a>
+          </button>
+          <button className="btn">
+            <a href="#">
+              Log-in<i className="fa-solid fa-arrow-right"></i>
+            </a>
+          </button>
+        </div>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+
