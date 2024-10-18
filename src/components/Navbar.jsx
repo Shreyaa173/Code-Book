c045ea4d8349ca93ba82df95047f274c8f27a893
srishti <srishtichoudhary52@gmail.com>
2024-10-19 00:36:23 +0530

    commit 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,46 +14,44 @@ function Navbar() {
 
   return (
     <nav>
-<ProgressBar />
-<div className="nav-up">
-  <div className="nav-icon">
-    <img src={Logo} alt="Code Book Logo" className="logo" />
-    <p>Code Book</p>
-  </div>
-  <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-    <i className={`fa ${isOpen ? 'fa-times' : 'fa-bars'}`} aria-hidden="true"></i>
-  </div>
-</div>
-
-<div className={`nav-content ${isOpen ? 'show' : ''}`}>
-  <ul className="nav-list">
-    <li className="nav-item">
-      <Link to="/login">Courses</Link>
-    </li>
-    
-          </li>
-          <li className='nav-item'>
-            <a href='#testimonal'>Testimonials</a>
-          </li>
-          <li className='nav-item'>
-            <a href='#contact'>Contact</a>
-          </li>
-        </ul>
-<div className="nav-button">
-  <button className="btn">
-    <Link to="/signup">
-      Sign-up <i className="fa-solid fa-arrow-right"></i>
-    </Link>
-  </button>
-  <button className="btn">
-    <Link to="/login">
-      Log-in <i className="fa-solid fa-arrow-right"></i>
-    </Link>
-  </button>
-</div>
-
+      <ProgressBar />
+      <div className="nav-up">
+        <div className="nav-icon">
+          <img src={Logo} alt="Code Book Logo" className="logo" />
+          <p>Code Book</p>
+        </div>
+        <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <i className={`fa ${isOpen ? 'fa-times' : 'fa-bars'}`} aria-hidden="true"></i>
+        </div>
       </div>
-    </nav>
+
+      <div className={`nav-content ${isOpen ? 'show' : ''}`}>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/login">Courses</Link>
+          </li>
+        <li className='nav-item'>
+          <a href='#testimonal'>Testimonials</a>
+        </li>
+        <li className='nav-item'>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+      <div className="nav-button">
+        <button className="btn">
+          <Link to="/signup">
+            Sign-up <i className="fa-solid fa-arrow-right"></i>
+          </Link>
+        </button>
+        <button className="btn">
+          <Link to="/login">
+            Log-in <i className="fa-solid fa-arrow-right"></i>
+          </Link>
+        </button>
+      </div>
+
+    </div>
+    </nav >
   );
 }
 
