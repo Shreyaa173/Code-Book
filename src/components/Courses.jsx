f227063b80019062cb66ae5c15f8c5fed1aa91f1
Aman Reddy p <reddyaman77.ar@gmail.com>
2024-10-19 00:47:28 +0530

    add search icon in explore course. #271

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import CourseCards from './CourseCards'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 function Courses() {
   return (
     <div>
-        <div className="course-head" id="course-head">
-            <div className="search-icon">
-                <h2>Our Courses</h2>
-                <div className="search-bar">
-                    <input type="text" placeholder="Explore Courses..." spellcheck="false"/>
-                    <button className='mag'><i className="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-            </div>
-        </div>  
-        <CourseCards></CourseCards>
+      <div className="course-head" id="course-head">
+        <div className="search-icon">
+          <h2>Our Courses</h2>
+          <div className="search-bar">
+            <input 
+              type="text" 
+              placeholder="Explore Courses... " 
+              
+              spellCheck="false" 
+            /> 
+            <i className="fas fa-search" style={{position:"relative"}}></i>
+           
+          </div>
+        </div>
+      </div>  
+      <CourseCards />
     </div>
   )
 }
