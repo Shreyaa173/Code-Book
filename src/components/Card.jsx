45a9161bac572fe73c3b456efc03c8ba36d751d2
Kiran-Jadhav200 <jadhavrkiran21@gmail.com>
2024-10-20 10:43:44 +0530

    issue no #126 #295 Completed Added Books link and Docs Link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 export default function Card({ imgurl, coursename, authorname, lessons, ytlink, docsLink, bookslink }) {
   return (
-    <div className="course-box">
+    //ytlink prop into the Card component 
+    //target="_blank" attribute in the anchor tag to open the course link in a new tab
+    <div  className="course-box">
+    <a href={ytlink} target="_blank" rel="noopener noreferrer">
       <div className="course-image">
         <img src={imgurl} alt={coursename} />
       </div>
+      </a>
+      
       <div className="course-text">
+      <h4>{coursename}</h4>
         <p className="course-name">{coursename}</p>
         <p className="course-author"><i className="fa-solid fa-user-tie"></i>By {authorname}</p>
         <p className="lessons"><i className="fa-brands fa-leanpub"></i>{lessons} Lessons</p>
-        <div className="course-links">
-          <a className='youtube-link' href={ytlink} target="_blank" rel="noreferrer"><i class="fa-brands fa-youtube"></i>  youtube</a> <br/>
-          <a href={docsLink} target="_blank" rel="noreferrer" ><i class="fa-solid fa-book">  </i>   docs</a> <br/>
-          <a href={bookslink} target="_blank" rel="noreferrer"><i class="fa-solid fa-book">  </i>   Books</a>
-
-
-
-        </div>
+        <a href={docsLink} target="_blank" rel="noopener noreferrer" className="docs"> <i className="fa-solid fa-file-alt"></i> Docs</a>
+         <a href={bookslink} target="_blank" rel="noopener noreferrer" className="books"> <i className="fa-solid fa-book"></i> Books</a>
       </div>
-    </div>
+      </div>
   );
 }
