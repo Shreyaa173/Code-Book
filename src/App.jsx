04020bc97fbba1866874e4681fcf75217237290f
saras-69 <115482884+saras-69@users.noreply.github.com>
2024-10-23 18:30:47 +0530

    add-RecoverPassword-feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
-import Login from "./components/Login"; 
+import Login from "./components/Login";
 import Signup from "./components/Signup";
+import RecoverPasswordPage from "./components/RecoverPasswordPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
+        <Route path='/recover-password' element={<RecoverPasswordPage />} />
       </Routes>
     </Router>
   );
