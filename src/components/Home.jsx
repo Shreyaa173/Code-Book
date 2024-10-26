f8d0430af336abb05abe6fd9b9e7f6de4ffa100b
Aman-Jangid <aman2003jangid@gmail.com>
2024-10-26 20:33:34 +0530

    Add jsconfig.json

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Introduction from './Introduction'
+import React from "react";
+import Navbar from "./Navbar";
+import Introduction from "./Introduction";
 import { Courses, Slideshow, Contact, Footer } from "./index";
 function Home() {
   return (
     <div>
-        <Navbar />
-        <Introduction />
-        <Courses />
-        <Slideshow />
-        <Contact />
-        <Footer />
+      <Navbar />
+      <Introduction />
+      <Courses />
+      <Slideshow />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
