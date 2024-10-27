92cb3a8786b9357c6cb8e9939fbce507aa95656f
Akash Das <76689571+akash70629@users.noreply.github.com>
2024-10-27 12:02:23 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -49,6 +49,8 @@ export default function SignupPage() {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 required
+		pattern="[a-zA-Z ]+" oninvalid="this.setCustomValidity('Numbers and Symbols are not allowed')" 
+                oninput="this.setCustomValidity('')"
               />
             </div>
             <div className='input-group'>
