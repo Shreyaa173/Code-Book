70c781d93972f42143736344a2ce7dbc4b59a49b
harshbhar0629 <harshbhar.0629@gmail.com>
2024-10-29 09:45:31 +0530

    Edit course cards and make responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
+/** @format */
 
-export default function Card({ imgurl, coursename, authorname, notes, ytlink, gfgLink }) {
-  return (
-    <div className="course-box">
-      <div className="course-image">
-        {/* Link for the image to open the YouTube course */}
-        <a href={ytlink} target="_blank" rel="noopener noreferrer">
-          <img src={imgurl} alt={coursename} />
-        </a>
-      </div>
-      <div className="course-text">
-        <p className="course-name">{coursename}</p>
-        <p className="course-author">
-          <i className="fa-solid fa-user-tie"></i> By {authorname}
-        </p>
-        {/* Link for the Notes section to open in a new tab */}
-        <a href={gfgLink} target="_blank" rel="noopener noreferrer">
-          <p className="lessons">
-            <i className="fa-brands fa-leanpub"></i> {notes} Notes
-          </p>
-        </a>
-      </div>
-    </div>
-  );
+import React from "react";
+
+export default function Card({
+	imgurl,
+	coursename,
+	authorname,
+	notes,
+	ytlink,
+	gfgLink,
+}) {
+	return (
+		<div className="course-box">
+			<div className="course-image">
+				{/* Link for the image to open the YouTube course */}
+				<a
+					href={ytlink}
+					target="_blank"
+					rel="noopener noreferrer">
+					<img
+						src={imgurl}
+						alt={coursename}
+					/>
+				</a>
+			</div>
+			<div className="course-text">
+				<p className="course-name">{coursename}</p>
+				<p className="course-author">
+					<i className="fa-solid fa-user-tie"></i> By {authorname}
+				</p>
+				{/* Link for the Notes section to open in a new tab */}
+				<a
+					href={gfgLink}
+					target="_blank"
+					rel="noopener noreferrer">
+					<p className="lessons">
+						<i className="fa-brands fa-leanpub"></i> {notes} Notes
+					</p>
+				</a>
+			</div>
+		</div>
+	);
 }
